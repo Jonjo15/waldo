f87c57ace54fe37559e495d420f6fa5956d619f5
IVAN <ivanbatur5g@gmail.com>
2020-11-17 19:27:54 +0100

    Initial commit, firebase set up

---
+++ waldo/src/App.js
@@ -0,0 +1,15 @@
+// import {projectStorage, projectFirestore} from "./firebase/config.js"
+import React from "react"
+function App() {
+  // const test = (e) => {
+  //   console.log(projectStorage, projectFirestore)
+  // }
+  return (
+    <div className="App">
+      <h1>Where's Waldo?</h1>
+      {/* <button onClick={test}>Test</button> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ waldo/src/firebase/config.js
@@ -0,0 +1,26 @@
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+// import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/storage";
+import "firebase/firestore";
+var firebaseConfig = {
+    apiKey: "AIzaSyB9bL7ISHnLXh5xTMbcQTL_G_iMKjI7xAg",
+    authDomain: "waldo-543d1.firebaseapp.com",
+    databaseURL: "https://waldo-543d1.firebaseio.com",
+    projectId: "waldo-543d1",
+    storageBucket: "waldo-543d1.appspot.com",
+    messagingSenderId: "445902802665",
+    appId: "1:445902802665:web:73eb24674ca1164c8f2a78"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectFirestore = firebase.firestore()
+  const projectStorage = firebase.storage()
+
+  export {projectFirestore, projectStorage}
