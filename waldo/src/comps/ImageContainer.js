23ba6e69d35be6b292661849ac4b97d762916e0d
IVAN <ivanbatur5g@gmail.com>
2020-11-21 11:17:22 +0100

    Added some styles for waldo,odlaw,wiz divs

--- waldo/src/comps/ImageContainer.js
+++ waldo/src/comps/ImageContainer.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react"
+import React, {useState} from "react"
 // import useFirestore from "../hooks/useFirestore"
 // import {getValidDelta} from "../functions/getValidDelta"
 
